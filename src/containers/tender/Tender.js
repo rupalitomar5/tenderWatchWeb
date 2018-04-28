793ed4c35445377c999f7ab4c85909fe728d408c
rupalitomar5 <rupalitomar5@gmail.com>
2018-04-28 11:33:47 +0530

    css change of upload tender

--- src/containers/tender/Tender.js
+++ src/containers/tender/Tender.js
@@ -22,69 +22,75 @@ class Tender extends React.Component {
         debugger;
         return (
             <div className="login">
-                <div className="login-content">
+                <div className="col-lg-12 ml-auto p-5 hide">
                     <div className="login-logo">
                         <h3 className='colorText'>Tender:</h3>
                     </div>
                     <div className="login-form">
                         <ListGroup>
+                            <div className='row'>
+                            <div className='col-sm-6'>
+                                <ListGroupItemHeading className='colorText'>Tender Image</ListGroupItemHeading>
+                                <ListGroupItemText>
+                                    <div className='smallImage'>
+                                        <img src={this.props.tender && this.props.tender.tenderPhoto} onError={(e) => {
+                                            e.target.src = '/images/picture.svg'
+                                        }}/>
+                                    </div>
+                                </ListGroupItemText>
+                                <ListGroupItemHeading className='colorText'>Tender name:</ListGroupItemHeading>
+                                <ListGroupItemText className='colorText'>
+                                    {this.props.tender && this.props.tender.tenderName}
+                                </ListGroupItemText>
+                                <ListGroupItemHeading className='colorText'>Tender Description:</ListGroupItemHeading>
+                                <ListGroupItemText className='colorText'>
+                                    {this.props.tender && this.props.tender.description}
+                                </ListGroupItemText>
+                                <ListGroupItemHeading className='colorText'>Country:</ListGroupItemHeading>
+                                <ListGroupItemText className='colorText'>
+                                    {this.props.tender && this.props.tender.country &&
+                                    <React.Fragment><img
+                                        src={`data:image/png;base64,${this.props.tender.country.imageString}`} alt=''/>
+                                        {' '}{this.props.tender.country.countryName}</React.Fragment>}
+                                </ListGroupItemText>
+                                <ListGroupItemHeading className='colorText'>Category:</ListGroupItemHeading>
+                                <ListGroupItemText>
+                                    {this.props.tender && this.props.tender.category && <React.Fragment>
+                                        <img src={`data:image/png;base64,${this.props.tender.category.imgString}`} alt=''/>
+                                        {' '}{this.props.tender.category.categoryName}</React.Fragment>}
+                                </ListGroupItemText>
+                                <ListGroupItemHeading className='colorText'>Expiry Date:</ListGroupItemHeading>
+                                <ListGroupItemText>
+                                    {this.props.tender && this.props.tender.expiryDate}
+                                </ListGroupItemText>
+                            </div>
+                            <div className='col-sm-6'>
 
-                            <ListGroupItemHeading className='colorText'>Tender Image</ListGroupItemHeading>
-                            <ListGroupItemText>
-                                <div className='smallImage'>
-                                    <img src={this.props.tender && this.props.tender.tenderPhoto} onError={(e) => {
-                                        e.target.src = '/images/picture.svg'
-                                    }}/>
-                                </div>
-                            </ListGroupItemText>
-                            <ListGroupItemHeading className='colorText'>Tender name:</ListGroupItemHeading>
-                            <ListGroupItemText className='colorText'>
-                                {this.props.tender && this.props.tender.tenderName}
-                            </ListGroupItemText>
-                            <ListGroupItemHeading className='colorText'>Tender Description:</ListGroupItemHeading>
-                            <ListGroupItemText className='colorText'>
-                                {this.props.tender && this.props.tender.description}
-                            </ListGroupItemText>
-                            <ListGroupItemHeading className='colorText'>Country:</ListGroupItemHeading>
-                            <ListGroupItemText className='colorText'>
-                                {this.props.tender && this.props.tender.country &&
-                                <React.Fragment><img
-                                    src={`data:image/png;base64,${this.props.tender.country.imageString}`} alt=''/>
-                                    {this.props.tender.country.countryName}</React.Fragment>}
-                            </ListGroupItemText>
-                            <ListGroupItemHeading className='colorText'>Category:</ListGroupItemHeading>
-                            <ListGroupItemText>
-                                {this.props.tender && this.props.tender.category && <React.Fragment>
-                                    <img src={`data:image/png;base64,${this.props.tender.category.imgString}`} alt=''/>
-                                    {this.props.tender.category.categoryName}</React.Fragment>}
-                            </ListGroupItemText>
-                            <ListGroupItemHeading className='colorText'>Expiry Date:</ListGroupItemHeading>
-                            <ListGroupItemText>
-                                {this.props.tender && this.props.tender.expiryDate}
-                            </ListGroupItemText>
-                            <div style={{border:'1px solid black'}}/>
-                            <h4>Client Details:</h4>
-                            <div style={{border:'1px solid black'}}/>
-                            <ListGroupItemHeading className='colorText'>Email:</ListGroupItemHeading>
-                            <ListGroupItemText>
-                                {this.props.tender && this.props.tender.email}
-                            </ListGroupItemText>
-                            <ListGroupItemHeading className='colorText'>Contact no:</ListGroupItemHeading>
-                            <ListGroupItemText>
-                                {this.props.tender && this.props.tender.contactNo}
-                            </ListGroupItemText>
-                            <ListGroupItemHeading className='colorText'>Landline:</ListGroupItemHeading>
-                            <ListGroupItemText>
-                                {this.props.tender && this.props.tender.landlineNo}
-                            </ListGroupItemText>
-                            <ListGroupItemHeading className='colorText'>Address:</ListGroupItemHeading>
-                            <ListGroupItemText>
-                                {this.props.tender && this.props.tender.address}
-                            </ListGroupItemText>
-                            <ListGroupItemHeading className='colorText'>City:</ListGroupItemHeading>
-                            <ListGroupItemText>
-                                {this.props.tender && this.props.tender.city}
-                            </ListGroupItemText>
+                                <div style={{border:'1px solid black'}}/>
+                                <h4>Client Details:</h4>
+                                <div style={{border:'1px solid black'}}/>
+                                <ListGroupItemHeading className='colorText'>Email:</ListGroupItemHeading>
+                                <ListGroupItemText>
+                                    {this.props.tender && this.props.tender.email}
+                                </ListGroupItemText>
+                                <ListGroupItemHeading className='colorText'>Contact no:</ListGroupItemHeading>
+                                <ListGroupItemText>
+                                    {this.props.tender && this.props.tender.contactNo}
+                                </ListGroupItemText>
+                                <ListGroupItemHeading className='colorText'>Landline:</ListGroupItemHeading>
+                                <ListGroupItemText>
+                                    {this.props.tender && this.props.tender.landlineNo}
+                                </ListGroupItemText>
+                                <ListGroupItemHeading className='colorText'>Address:</ListGroupItemHeading>
+                                <ListGroupItemText>
+                                    {this.props.tender && this.props.tender.address}
+                                </ListGroupItemText>
+                                <ListGroupItemHeading className='colorText'>City:</ListGroupItemHeading>
+                                <ListGroupItemText>
+                                    {this.props.tender && this.props.tender.city}
+                                </ListGroupItemText>
+                            </div>
+                            </div>
                         </ListGroup>
                     </div>
                 </div>
--- src/containers/uploadTender/uploadTender.js
+++ src/containers/uploadTender/uploadTender.js
@@ -8,7 +8,6 @@ import {
 } from 'reactstrap';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
-import {NavLink, withRouter} from 'react-router-dom';
 import {getCountries, getCategories} from '../../actionMethods/userActionMethods';
 import {uploadTenderMethod} from '../../actionMethods/tenderActionMethods';
 import SpinnerLoader from '../../components/spinnerLoader/spinnerLoader';
@@ -20,12 +19,12 @@ class UploadTender extends React.Component {
         super(props);
         this.state = {
             fields: {
-                email:props.user.email,
+                email: props.user.email,
 
             },
             page: 1,
             errors: {
-                tenderName:""
+                tenderName: ""
             }
         }
     }
@@ -45,23 +44,19 @@ class UploadTender extends React.Component {
         }
     };
 
-    changePage = (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        const {fields} = this.state;
-        let flag = 0;
-        if(!fields.tenderName){
-            this.props.openAlertModal({header: 'Register', message: 'Please enter valid details'});
-        }else{
-            this.setState({page: +e.target.id});
-        }
-    };
-
     submitHandler = (e) => {
         e.preventDefault();
         e.stopPropagation();
-        debugger;
-        this.props.uploadTenderMethod(this.state.fields);
+        const {fields,errors} = this.state;
+
+        if (!fields.tenderName || !fields.email || errors.email) {
+            this.props.openAlertModal({header: 'Register', message: 'Please enter valid details'});
+        }else if(!fields.agree){
+            this.props.openAlertModal({header: 'Register', message: 'please check on agree'});
+        }
+        else {
+            this.props.uploadTenderMethod(this.state.fields);
+        }
     };
 
     validate = (e) => {
@@ -93,126 +88,115 @@ class UploadTender extends React.Component {
         const {errors} = this.state;
         return (
             <div className='col-lg-12 ml-auto p-5 hide'>
-                {this.props.isLoading && <SpinnerLoader/>}
+                {this.props.isLoading && <SpinnerLoader />}
                 {this.props.alertModal.isAlert &&
                 <AlertModal alertModal={this.props.alertModal}/>}
                 <h1 className='colorText'>Upload Tender:</h1>
                 <div className='login-form'>
                     <Form encType='multipart/form-data' onSubmit={this.submitHandler}>
-                        {this.state.page === 1 ?
-                            <React.Fragment>
-                                <FormGroup>
-                                    <Label>Tender Name*</Label>
-                                    <Input name='tenderName' type='text' onChange={this.changeHandler}
-                                           onBlur={this.validate}
-                                           value={this.state.fields.tenderName}
-                                    />
-                                    {errors.tenderName && <Alert color='danger'>{errors.tenderName}</Alert>}
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label> Tender Description </Label>
-                                    <Input name='description' type='text' onChange={this.changeHandler}
-                                           value={this.state.fields.description || ''}
-                                    />
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label>Country</Label>
-                                    <Input type='select' name='country' onChange={this.changeHandler}
-                                           onBlur={this.validate}
-                                           value={this.state.fields.country}
-                                    >
-                                        <option>Select one</option>
-                                        {
-                                            this.props.formData.countries &&
-                                            this.props.formData.countries.map((country, index) => (
-                                                <option key={index} value={country._id}><img
-                                                    src={`data:image/png;base64,${country.imageString}`}
-                                                    alt='tender image'/>{country.countryName}</option>
-                                            ))
-                                        }
-                                    </Input>
-                                    {/*<UncontrolledDropdown>
-                                        <DropdownToggle caret>
-                                            Country
-                                        </DropdownToggle>
-                                        <DropdownMenu>
-                                            {
-                                                this.props.formData.countries &&
-                                                this.props.formData.countries.map( (country, index) =>(
-                                                    <DropdownItem key={index} value={country.countryName}><img src={`data:image/png;base64,${country.imageString}`} alt='asds'/>{country.countryName}</DropdownItem>
-                                                ))
-                                            }
-                                        </DropdownMenu>
-                                    </UncontrolledDropdown>*/}
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label>City</Label>
-                                    <Input type='text' name='city' onChange={this.changeHandler}
-                                           value={this.state.fields.city}
-                                    />
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label>Categories</Label>
-                                    <Input type='select' name='category' onChange={this.changeHandler}
-                                           onBlur={this.validate}
-                                           value={this.state.fields.category}
-                                    >
-                                        <option>Select one</option>
-                                        {
-                                            this.props.formData.categories &&
-                                            this.props.formData.categories.map((category, index) => (
-                                                <option key={index} value={category._id}><img
-                                                    src={`data:image/png;base64,${category.imageString}`}
-                                                    alt='category image'/>{category.categoryName}</option>
-                                            ))
-                                        }
-                                    </Input>
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label>image</Label>
-                                    <Input type='file' name='image' onChange={this.changeHandler}
-                                    />
-                                </FormGroup>
-                                <button id={2} className='btn btnAll' onClick={this.changePage}>next</button>
-                            </React.Fragment> :
-                            <React.Fragment>
-                                <h3 className='colorText'>How contractor can contact you?</h3>
-                                <FormGroup>
-                                    <Label> Email* </Label>
-                                    <Input type='text' name='email' onChange={this.changeHandler}
-                                           onBlur={this.validate}
-                                           value={this.state.fields.email || ''}/>
-                                    {errors.email && <Alert color='danger'>{errors.email}</Alert>}
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label> Mobile Number </Label>
-                                    <Input type='number' name='contactNo' onChange={this.changeHandler}
-                                           onBlur={this.validate}
-                                           value={this.state.fields.contactNo}
-                                    />
-                                    {errors.contactNo && <Alert color='danger'>{errors.contactNo}</Alert>}
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label> Landline Number </Label>
-                                    <Input type='number' name='landlineNo' onChange={this.changeHandler}
-                                           value={this.state.fields.landlineNo}
-                                    />
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label> Address </Label>
-                                    <Input type='textarea' name='address' onChange={this.changeHandler}
-                                           value={this.state.fields.address}
-                                    />
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label><input type='checkbox' name='agree' onChange={this.changeHandler}/></Label>
-                                    <Label> I Agree </Label>
-                                </FormGroup>
-                                <button id={1} className='btn btnAll' onClick={this.changePage}>Previous</button>
-                                {' '}
-                                <button className='btn btnAll'>submit</button>
-                            </React.Fragment>
-                        }
+                        <div className='row'>
+                        <div className='col-sm-6'>
+                            <h3 className='colorText'>tender info:</h3>
+                            <FormGroup>
+                                <Input type='file' name='image' onChange={this.changeHandler}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>Tender Name*</Label>
+                                <Input name='tenderName' type='text' onChange={this.changeHandler}
+                                       onBlur={this.validate}
+                                       value={this.state.fields.tenderName}
+                                />
+                                {errors.tenderName && <Alert color='danger'>{errors.tenderName}</Alert>}
+                            </FormGroup>
+                            <FormGroup>
+                                <Label> Tender Description </Label>
+                                <Input name='description' type='text' onChange={this.changeHandler}
+                                       value={this.state.fields.description || ''}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>Country</Label>
+                                <Input type='select' name='country' onChange={this.changeHandler}
+                                       onBlur={this.validate}
+                                       value={this.state.fields.country}
+                                >
+                                    <option>Select one</option>
+                                    {
+                                        this.props.formData.countries &&
+                                        this.props.formData.countries.map((country, index) => (
+                                            <option key={index} value={country._id}><img
+                                                src={`data:image/png;base64,${country.imageString}`}
+                                                alt='tender image'/>{country.countryName}</option>
+                                        ))
+                                    }
+                                </Input>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>City</Label>
+                                <Input type='text' name='city' onChange={this.changeHandler}
+                                       value={this.state.fields.city}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>Categories</Label>
+                                <Input type='select' name='category' onChange={this.changeHandler}
+                                       onBlur={this.validate}
+                                       value={this.state.fields.category}
+                                >
+                                    <option>Select one</option>
+                                    {
+                                        this.props.formData.categories &&
+                                        this.props.formData.categories.map((category, index) => (
+                                            <option key={index} value={category._id}><img
+                                                src={`data:image/png;base64,${category.imageString}`}
+                                                alt='category image'/>{category.categoryName}</option>
+                                        ))
+                                    }
+                                </Input>
+                            </FormGroup>
+                        </div>
+                        <div className='col-sm-6'>
+                            <h3 className='colorText'>How contractor can contact you?</h3>
+                            <FormGroup>
+                                <Label> Email* </Label>
+                                <Input type='text' name='email' onChange={this.changeHandler}
+                                       onBlur={this.validate}
+                                       value={this.state.fields.email || ''}/>
+                                {errors.email && <Alert color='danger'>{errors.email}</Alert>}
+                            </FormGroup>
+                            <FormGroup>
+                                <Label> Mobile Number </Label>
+                                <Input type='number' name='contactNo' onChange={this.changeHandler}
+                                       onBlur={this.validate}
+                                       value={this.state.fields.contactNo}
+                                />
+                                {errors.contactNo && <Alert color='danger'>{errors.contactNo}</Alert>}
+                            </FormGroup>
+                            <FormGroup>
+                                <Label> Landline Number </Label>
+                                <Input type='number' name='landlineNo' onChange={this.changeHandler}
+                                       value={this.state.fields.landlineNo}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label> Address </Label>
+                                <Input type='textarea' name='address' onChange={this.changeHandler}
+                                       value={this.state.fields.address}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>
+                                    <input type='checkbox' name='agree' onChange={this.changeHandler}
+                                           onBlur={this.validate}/>
+                                </Label>
+                                <Label> I Agree </Label>
+                                {errors.agree && <Alert color='danger'>{errors.agree}</Alert>}
+                            </FormGroup>
+                            {' '}
+                            <button className='btn btnAll'>submit</button>
+                        </div>
+                        </div>
                     </Form>
                 </div>
                 <form>
@@ -227,7 +211,7 @@ const mapStateToProps = (state) => {
         formData: state.formData,
         isLoading: state.isLoading,
         alertModal: state.alertModal,
-        user:state.auth.user
+        user: state.auth.user
     }
 };
 const mapDispatchToProps = (dispatch) => bindActionCreators({
